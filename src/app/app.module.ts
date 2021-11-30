ec31a209841e7ad044212b654d9b890beb4c8362
Valerio <Valerio@no.com>
2021-11-30 16:15:29 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina2Component,
+    Pagina1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagina1/pagina1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styleUrls: ['./pagina1.component.css']
+})
+export class Pagina1Component implements OnInit {
+  valore = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  incrementa(){
+    this.valore+=1;
+  }
+
+}
